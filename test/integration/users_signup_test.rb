3d03370cfc601eb28cc270498e33860e6f6e9d20
Keitaro <keitaro.cryptocurrency@gmail.com>
2020-05-04 18:23:42 +0900

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" } }
+    end
+    
+    assert_template 'users/new'
+    #id=error_explanationのdivタグが存在するかどうか？
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+    #ulタグの中のliタグの中に右の文字列が入っているかどうか
+    assert_select 'ul' do
+      assert_select 'li', 'Name can\'t be blank'
+      assert_select 'li', 'Email is invalid'
+      assert_select 'li', 'Password confirmation doesn\'t match Password'
+      #パスワードの文字数制限が変わった時のため、文字数部分のエラーメッセージより前で部分一致を検証
+      assert_select 'li', /Password is too short*/
+    end
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
+end
