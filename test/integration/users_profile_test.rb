b7d15236f3adb3a64ae1b827628043c922916b78
Keitaro <keitaro.cryptocurrency@gmail.com>
2020-05-14 19:20:34 +0900

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -25,10 +25,25 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar'
+
+    assert_select 'strong#following' # HTMLを網羅的にチェックするテストは壊れやすいから気をつける
+    assert_match @user.following.count.to_s, response.body 
+    assert_select 'strong#followers'
+    assert_match @user.followers.count.to_s, response.body 
+    
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination', count: 1
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+  end
+
+  test "home when logged in" do
+    log_in_as(@user)
+    get root_path
+    assert_select 'strong#following'
+    assert_match @user.following.count.to_s, response.body 
+    assert_select 'strong#followers'
+    assert_match @user.followers.count.to_s, response.body 
   end
 end
